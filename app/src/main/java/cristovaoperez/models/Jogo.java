d302361bd62fe0a1f23ccdf81e4600e2e9bcdca8
Cristo378 <cristovaohenrique157@gmail.com>
2022-11-07 17:18:39 +0000

    Games

---
+++ app/src/main/java/cristovaoperez/models/Jogo.java
@@ -0,0 +1,28 @@
+package cristovaoperez.models;
+
+public class Jogo {
+    private int id;
+    private String titulo;
+
+    private int IdGenero;
+
+    private void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return this.id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getTitulo() {
+        return this.titulo;
+    }
+    public void setIdGenero(int idGenero) {
+        this.idGenero = idGenero;
+    }
+    public int getIdGenero() {
+        return this.idGenero;
+    }
+}
