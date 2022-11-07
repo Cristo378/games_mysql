9bc4426055bf65a903bf789cfb27f2f9e45f1c7a
Cristo378 <cristovaohenrique157@gmail.com>
2022-11-07 16:50:01 +0000

    Games

---
+++ app/src/main/java/cristovaoperez/repositories/GeneroRepository.java
@@ -0,0 +1,9 @@
+package cristovaoperez.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import cristovaoperez.models.Genero;
+
+public interface GeneroRepository extends CrudRepository<Genero, Integer> {
+    
+}
--- app/src/main/webapp/index.jsp
+++ app/src/main/webapp/index.jsp
@@ -9,5 +9,4 @@
     <body>
         <h1>Home Page</h1>
     </body>
-
-    
+</html>
